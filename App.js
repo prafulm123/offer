dc7d9f58f671ed17a7d20100f41e7c93d6f962f3
praful-maurya <prafulfernandes4@gmail.com>
2026-05-08 12:14:42 +0530

    app.js

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import AppNavigator from './src/screens/navigation/NavigationStack';
+import FormProvider from './src/context/FormContext';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <FormProvider>
+        <StatusBar style="auto" />
+        <AppNavigator />
+      </FormProvider>
     </View>
   );
 }
@@ -14,7 +18,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
